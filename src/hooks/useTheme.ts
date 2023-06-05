6468d58c9a3c4c0e8d06d1c6bd0ddc5d5e31820d
Facundo14 <facundotanovich@gmail.com>
2023-06-04 22:47:36 -0300

    Update useTheme.ts

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,49 +1,32 @@
-import { useEffect, useState } from "react";
-
-const getInitialThemeState = () => {
-  if (typeof window !== 'undefined') {
-    const storedTheme = localStorage.getItem('theme-informatteo') as 'light' | 'dark';
-    if (storedTheme) {
-      return storedTheme;
+const initialThemeState = () => {
+    if(typeof window !== 'undefined'){
+        if (localStorage.getItem('theme-informatteo')) {
+            return localStorage.getItem('theme-informatteo') as 'light' | 'dark'
+        }
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
     }
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-  }
-  return 'light';
+    return 'light'
 }
 
 export const useTheme = () => {
-  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const [theme, setTheme] = useState<'light' | 'dark'>(prefersDarkMode ? 'dark' : 'light');
 
-  useEffect(() => {
-    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const [theme, setTheme] = useState<'light' | 'dark'>(initialThemeState);
 
-    const handleDarkModeChange = (event) => {
-      const newTheme = event.matches ? 'dark' : 'light';
-      setTheme(newTheme);
-    };
+    useEffect(() => {
+        if (theme === 'light') {
+            document.documentElement.classList.remove('dark');
+        } else {
+            document.documentElement.classList.add('dark');
+        }
+        localStorage.setItem('theme-informatteo', theme);
+    }, [theme])
 
-    darkModeMediaQuery.addListener(handleDarkModeChange);
-    return () => {
-      darkModeMediaQuery.removeListener(handleDarkModeChange);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (theme === 'light') {
-      document.documentElement.classList.remove('dark');
-    } else {
-      document.documentElement.classList.add('dark');
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
     }
-    localStorage.setItem('theme-informatteo', theme);
-  }, [theme]);
 
-  const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  };
-
-  return {
-    theme,
-    toggleTheme
-  };
-};
+    return {
+        theme,
+        toggleTheme
+    }
+}
