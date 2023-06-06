1c8a86af0bf5d36145f2a54db49d535bbac00c14
Facundo Tanovich <facundotanovich@gmail.com>
2023-06-05 23:13:42 -0300

    Agregado el footer que se quede abajo siempre

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,10 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/logo.ico" />
       </head>
-      <body className={`${raleway.className} bg-slate-100 dark:bg-slate-900 scrollbar-thin scrollbar-thumb-red-600 scrollbar-rounded sm:mx-4 md:mx-8 lg:mx-12 xl:mx-24`} >
+      <body className={`${raleway.className} flex flex-col h-screen bg-slate-100 dark:bg-slate-900 scrollbar-thin scrollbar-thumb-red-600 scrollbar-rounded sm:mx-4 md:mx-8 lg:mx-12 xl:mx-24`} >
         <Providers>
           <Navegacion />
-          <main className="flex mx-7 dark:text-white flex-col items-center justify-between sm:mx-0 md:mx-4 lg:mx-6 xl:mx-12">
+          <main className=" flex-grow mx-7 dark:text-white flex-col items-center justify-between sm:mx-0 md:mx-4 lg:mx-6 xl:mx-12">
             {children}
           </main>
 
--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800 static bottom-0 sm:mx-0 md:mx-4 lg:mx-6">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                 <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://informatteo.com.ar/" className="hover:underline">InforMatteo™</a>. Todos los Derechos Reservados.
                 </span>
